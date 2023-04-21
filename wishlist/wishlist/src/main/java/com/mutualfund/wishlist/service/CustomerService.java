3a93031ed0a2c22084565184a077acd8378834f8
Venkatesh Golla <venkatesh.golla@hcl.com>
2023-04-20 19:55:58 -0700

    removed wishlist which was mistakenly added to main branch

--- wishlist/wishlist/src/main/java/com/mutualfund/wishlist/service/CustomerService.java
+++
@@ -1,7 +0,0 @@
-package com.mutualfund.wishlist.service;
-
-import com.mutualfund.wishlist.model.Customer;
-
-public interface CustomerService {
-	Customer getCustomer(int customerId) throws Exception;
-}	
